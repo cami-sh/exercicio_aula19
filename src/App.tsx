901e57222b7f24f7f7f059d550e0ef5c64cf311f
Camila Sayuri <camilahanazono@gmail.com>
2021-02-01 21:56:28 -0300

    recebendo os inputs e enviando para a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  let inputNome = useRef<HTMLInputElement>(null)
+  let inputIdade = useRef<HTMLInputElement>(null)
+  let inputEmpresa = useRef<HTMLInputElement>(null)
+  let inputPhone = useRef<HTMLInputElement>(null)
+
+  const enviarDados = () => {
+
+    const meuNome = inputNome.current?.value
+    const minhaIdade = inputIdade.current?.value
+    const minhaEmpresa = inputEmpresa.current?.value
+    const meuTelefone = inputPhone.current?.value
+
+    axios.post("http://localhost:4000/usuarios", {
+      name: meuNome,
+      age: minhaIdade,
+      company: minhaEmpresa,
+      phone: meuTelefone,
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input type="text" ref = {inputNome} placeholder='Nome'/>
+      <input type="text" ref = {inputIdade} placeholder='Idade'/>
+      <input type="text" ref = {inputEmpresa} placeholder='Empresa'/>
+      <input type="text" ref = {inputPhone} placeholder='Telefone'/>
+
+      <button onClick={enviarDados}>Enviar</button>
+
     </div>
   );
 }
